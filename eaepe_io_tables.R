cf0e2c3fe8d1bce0aa243f61ff64c27c3273f4ef
Marco Veronese Passarella <marco.veronese.passarella@gmail.com>
2025-02-04 11:59:04 +0100

    Update eaepe_io_tables.R

--- eaepe_io_tables.R
+++ eaepe_io_tables.R
@@ -1,18 +1,25 @@
-# BS, TFM AND IO TABLES
+# Model PC for R: balance-sheet and transactions-flow matrix
+# from Wynne Godley and Marc Lavoie
+# Monetary Economics
+# Chapter 4 
 
-# Created by Marco Veronese Passarella
+# Version: 2 November 2023
 
-# Version: 7 June 2024; revised: 27 January 2025
+################################################################################
 
-### Prepare the environment ################################################################################
+#Create BS and TFM tables
 
 #Upload libraries
 library(knitr)
 
 #Choose a year
-yr=20
+yr=50
 
-### BS matrix ################################################################################
+#Choose a scenario (note: 1 = baseline)
+scen=1
+
+################################################################################
+################################################################################
 
 #Create row names for BS matrix
 rownames<-c( "Cash (money)",
@@ -20,11 +27,13 @@ rownames<-c( "Cash (money)",
              "Wealth",
              "Column total")
 
+################################################################################
+
 #Create households aggregates
-H <-c( round(h_h[1,yr], digits = 2),                                                                    
-       round(b_h[1,yr], digits = 2),                                                                    
-       round(-v[1,yr], digits = 2),                                                                    
-       round(-v[1,yr]+h_h[1,yr]+b_h[1,yr], digits = 2)
+H <-c( round(h_h[scen,yr], digits = 2),                                                                    
+       round(b_h[scen,yr], digits = 2),                                                                    
+       round(-v[scen,yr], digits = 2),                                                                    
+       round(-v[scen,yr]+h_h[scen,yr]+b_h[scen,yr], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -32,6 +41,8 @@ H_BS<-as.data.frame(H,row.names=rownames)
 
 #Print firms column
 kable(H_BS)
+
+################################################################################
 
 #Create firms aggregates
 P <-c( "",                                                                    
@@ -46,10 +57,12 @@ F_BS<-as.data.frame(P,row.names=rownames)
 #Print firms column
 kable(F_BS)
 
+################################################################################
+
 #Create government aggregates
 G   <-c( "",                                                                    
-         round(-b_s[1,yr], digits = 2),                                                                    
-         round(b_s[1,yr], digits = 2),                                                                    
+         round(-b_s[scen,yr], digits = 2),                                                                    
+         round(b_s[scen,yr], digits = 2),                                                                    
          "0"
 )                                                                    
 
@@ -59,11 +72,13 @@ G_BS<-as.data.frame(G,row.names=rownames)
 #Print firms column
 kable(G_BS)
 
+################################################################################
+
 #Create CB aggregates
-CB  <-c( round(-h_s[1,yr], digits = 2),                                                                    
-         round(b_cb[1,yr], digits = 2),                                                                    
+CB  <-c( round(-h_s[scen,yr], digits = 2),                                                                    
+         round(b_cb[scen,yr], digits = 2),                                                                    
          "0",
-         round(-h_s[1,yr]+b_cb[1,yr], digits = 2)
+         round(-h_s[scen,yr]+b_cb[scen,yr], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -72,13 +87,15 @@ CB_BS<-as.data.frame(CB,row.names=rownames)
 #Print firms column
 kable(CB_BS)
 
+################################################################################
+
 #Create "row total" column
-Tot  <-c( round(h_h[1,yr]-h_s[1,yr], digits = 2),                                                                    
-          round(b_h[1,yr]+b_cb[1,yr]-b_s[1,yr], digits = 2),                                                                    
-          round(-v[1,yr]+b_s[1,yr], digits = 2),
-          round(h_h[1,yr]-h_s[1,yr]+
-                  b_h[1,yr]+b_cb[1,yr]-b_s[1,yr]+
-                  -v[1,yr]+b_s[1,yr], digits = 2)
+Tot  <-c( round(h_h[scen,yr]-h_s[scen,yr], digits = 2),                                                                    
+          round(b_h[scen,yr]+b_cb[scen,yr]-b_s[scen,yr], digits = 2),                                                                    
+          round(-v[scen,yr]+b_s[scen,yr], digits = 2),
+          round(h_h[scen,yr]-h_s[scen,yr]+
+                b_h[scen,yr]+b_cb[scen,yr]-b_s[scen,yr]+
+                -v[scen,yr]+b_s[scen,yr], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -87,11 +104,14 @@ Tot_BS<-as.data.frame(Tot,row.names=rownames)
 #Print firms column
 kable(Tot_BS)
 
+################################################################################
+
 #Create BS matrix
 BS_Matrix<-cbind(H_BS,F_BS,CB_BS,G_BS,Tot_BS)
 kable(BS_Matrix) #Unload kableExtra to use this
 
-### TFM matrix ################################################################################
+################################################################################
+################################################################################
 
 #Create row names for TFM
 rownames<-c( "Consumption",
@@ -104,17 +124,19 @@ rownames<-c( "Consumption",
              "Change in bills",
              "Column total")
 
+################################################################################
+
 #Create households aggregates
-H <-c( round(-cons[1,yr]*p_c[1,yr], digits = 2),
+H <-c( round(-cons[scen,yr], digits = 2),
        "",
-       round(y[1,yr], digits = 2),                                                                    
-       round(r[1,yr-1]*b_h[1,yr-1], digits = 2),
+       round(y[scen,yr], digits = 2),                                                                    
+       round(r[scen,yr-1]*b_h[scen,yr-1], digits = 2),
        "",
-       round(-t[1,yr], digits = 2),
-       round(-h_h[1,yr]+h_h[1,yr-1], digits = 2),
-       round(-b_h[1,yr]+b_h[1,yr-1], digits = 2),
-       round(-cons[1,yr]*p_c[1,yr]+y[1,yr]+r[1,yr-1]*b_h[1,yr-1]-t[1,yr]+
-               -h_h[1,yr]+h_h[1,yr-1]-b_h[1,yr]+b_h[1,yr-1], digits = 2)
+       round(-t[scen,yr], digits = 2),
+       round(-h_h[scen,yr]+h_h[scen,yr-1], digits = 2),
+       round(-b_h[scen,yr]+b_h[scen,yr-1], digits = 2),
+       round(-cons[scen,yr]+y[scen,yr]+r[scen,yr-1]*b_h[scen,yr-1]-t[scen,yr]+
+              -h_h[scen,yr]+h_h[scen,yr-1]-b_h[scen,yr]+b_h[scen,yr-1], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -123,16 +145,18 @@ H_TFM<-as.data.frame(H,row.names=rownames)
 #Print firms column
 kable(H_TFM)
 
+################################################################################
+
 #Create firms aggregates
-P <-c( round(cons[1,yr]*p_c[1,yr], digits = 2),
-       round(g[1,yr]*p_g[1,yr], digits = 2),
-       round(-y[1,yr], digits = 2),                                                                    
+P <-c( round(cons[scen,yr], digits = 2),
+       round(g[scen,yr], digits = 2),
+       round(-y[scen,yr], digits = 2),                                                                    
        "",
        "",
        "",
        "",
        "",
-       round(cons[1,yr]*p_c[1,yr]+g[1,yr]*p_g[1,yr]-y[1,yr], digits = 2)
+       round(cons[scen,yr]+g[scen,yr]-y[scen,yr], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -141,20 +165,22 @@ F_TFM<-as.data.frame(P,row.names=rownames)
 #Print firms column
 kable(F_TFM)
 
+################################################################################
+
 #Create government aggregates
 G   <-c( "",
-         round(-g[1,yr]*p_g[1,yr], digits = 2),
+         round(-g[scen,yr], digits = 2),
          "",                                                                    
-         round(-r[1,yr-1]*b_s[1,yr-1], digits = 2),
-         round(r[1,yr-1]*b_cb[1,yr-1], digits = 2),
-         round(t[1,yr], digits = 2),
+         round(-r[scen,yr-1]*b_s[scen,yr-1], digits = 2),
+         round(r[scen,yr-1]*b_cb[scen,yr-1], digits = 2),
+         round(t[scen,yr], digits = 2),
          "",
-         round(b_s[1,yr]-b_s[1,yr-1], digits = 2),
-         round(-g[1,yr]*p_g[1,yr]+
-                 -r[1,yr-1]*b_s[1,yr-1]+
-                 r[1,yr-1]*b_cb[1,yr-1]+
-                 t[1,yr]+
-                 b_s[1,yr]-b_s[1,yr-1], digits = 2)
+         round(b_s[scen,yr]-b_s[scen,yr-1], digits = 2),
+         round(-g[scen,yr]+
+               -r[scen,yr-1]*b_s[scen,yr-1]+
+                r[scen,yr-1]*b_cb[scen,yr-1]+
+                t[scen,yr]+
+                b_s[scen,yr]-b_s[scen,yr-1], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -163,16 +189,18 @@ G_TFM<-as.data.frame(G,row.names=rownames)
 #Print firms column
 kable(G_TFM)
 
+################################################################################
+
 #Create CB aggregates
 CB   <-c( "",
-          "",
-          "",                                                                    
-          round(r[1,yr-1]*b_cb[1,yr-1], digits = 2),
-          round(-r[1,yr-1]*b_cb[1,yr-1], digits = 2),
-          "",
-          round(h_s[1,yr]-h_s[1,yr-1], digits = 2),
-          round(-b_cb[1,yr]+b_cb[1,yr-1], digits = 2),
-          round(h_s[1,yr]-h_s[1,yr-1]-b_cb[1,yr]+b_cb[1,yr-1], digits = 2)
+         "",
+         "",                                                                    
+         round(r[scen,yr-1]*b_cb[scen,yr-1], digits = 2),
+         round(-r[scen,yr-1]*b_cb[scen,yr-1], digits = 2),
+         "",
+         round(h_s[scen,yr]-h_s[scen,yr-1], digits = 2),
+         round(-b_cb[scen,yr]+b_cb[scen,yr-1], digits = 2),
+         round(h_s[scen,yr]-h_s[scen,yr-1]-b_cb[scen,yr]+b_cb[scen,yr-1], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -181,23 +209,25 @@ CB_TFM<-as.data.frame(CB,row.names=rownames)
 #Print firms column
 kable(CB_TFM)
 
+################################################################################
+
 #Create "row total" column
-Tot   <-c(round(cons[1,yr]*p_c[1,yr]-cons[1,yr]*p_c[1,yr], digits = 2),
-          round(g[1,yr]*p_g[1,yr]-g[1,yr]*p_g[1,yr], digits = 2),
-          round(y[1,yr]-y[1,yr], digits = 2),                                                                    
-          round(r[1,yr-1]*b_h[1,yr-1]+r[1,yr-1]*b_cb[1,yr-1]-r[1,yr-1]*b_s[1,yr-1], digits = 2),
-          round(r[1,yr-1]*b_cb[1,yr-1]-r[1,yr-1]*b_cb[1,yr-1], digits = 2),
-          round(-t[1,yr]+t[1,yr], digits = 2),
-          round(-h_h[1,yr]+h_h[1,yr-1]+h_s[1,yr]-h_s[1,yr-1], digits = 2),
-          round(-b_h[1,yr]+b_h[1,yr-1]-b_cb[1,yr]+b_cb[1,yr-1]+b_s[1,yr]-b_s[1,yr-1], digits = 2),
-          round(cons[1,yr]*p_c[1,yr]-cons[1,yr]*p_c[1,yr]+
-                  g[1,yr]*p_g[1,yr]-g[1,yr]*p_g[1,yr]+
-                  y[1,yr]-y[1,yr]+
-                  r[1,yr-1]*b_h[1,yr-1]+r[1,yr-1]*b_cb[1,yr-1]-r[1,yr-1]*b_s[1,yr-1]+
-                  r[1,yr-1]*b_cb[1,yr-1]-r[1,yr-1]*b_cb[1,yr-1]+
-                  -t[1,yr]+t[1,yr]+
-                  -h_h[1,yr]+h_h[1,yr-1]+h_s[1,yr]-h_s[1,yr-1]+
-                  -b_h[1,yr]+b_h[1,yr-1]-b_cb[1,yr]+b_cb[1,yr-1]+b_s[1,yr]-b_s[1,yr-1], digits = 2)
+Tot   <-c(round(cons[scen,yr]-cons[scen,yr], digits = 2),
+          round(g[scen,yr]-g[scen,yr], digits = 2),
+          round(y[scen,yr]-y[scen,yr], digits = 2),                                                                    
+          round(r[scen,yr-1]*b_h[scen,yr-1]+r[scen,yr-1]*b_cb[scen,yr-1]-r[scen,yr-1]*b_s[scen,yr-1], digits = 2),
+          round(r[scen,yr-1]*b_cb[scen,yr-1]-r[scen,yr-1]*b_cb[scen,yr-1], digits = 2),
+          round(-t[scen,yr]+t[scen,yr], digits = 2),
+          round(-h_h[scen,yr]+h_h[scen,yr-1]+h_s[scen,yr]-h_s[scen,yr-1], digits = 2),
+          round(-b_h[scen,yr]+b_h[scen,yr-1]-b_cb[scen,yr]+b_cb[scen,yr-1]+b_s[scen,yr]-b_s[scen,yr-1], digits = 2),
+          round(cons[scen,yr]-cons[scen,yr]+
+                  g[scen,yr]-g[scen,yr]+
+                  y[scen,yr]-y[scen,yr]+
+                  r[scen,yr-1]*b_h[scen,yr-1]+r[scen,yr-1]*b_cb[scen,yr-1]-r[scen,yr-1]*b_s[scen,yr-1]+
+                  r[scen,yr-1]*b_cb[scen,yr-1]-r[scen,yr-1]*b_cb[scen,yr-1]+
+                  -t[scen,yr]+t[scen,yr]+
+                  -h_h[scen,yr]+h_h[scen,yr-1]+h_s[scen,yr]-h_s[scen,yr-1]+
+                  -b_h[scen,yr]+b_h[scen,yr-1]-b_cb[scen,yr]+b_cb[scen,yr-1]+b_s[scen,yr]-b_s[scen,yr-1], digits = 2)
 )                                                                    
 
 #Create table of results
@@ -206,68 +236,14 @@ Tot_TFM<-as.data.frame(Tot,row.names=rownames)
 #Print firms column
 kable(Tot_TFM)
 
+################################################################################
+
 #Create TFM matrix
 TFM_Matrix<-cbind(H_TFM,F_TFM,CB_TFM,G_TFM,Tot_TFM)
 kable(TFM_Matrix) #Unload kableExtra to use this
 
-### IO matrix ################################################################################
-
-#Create row names for IO matrix
-rownames <-c( "Inudstry 1 (production)",
-              "Industry 2 (production)",
-              "Value added",
-              "Output"
-)
-
-#Create industry 1 column
-Ind1 <- c(round(x[1,yr,1]*A[1]*p[1,yr,1], digits = 2),                                                                    
-          round(x[1,yr,1]*A[2]*p[1,yr,2], digits = 2),
-          round(x[1,yr,1]*p[1,yr,1]-(x[1,yr,1]*A[1]*p[1,yr,1]+x[1,yr,1]*A[2]*p[1,yr,2]), digits = 2),
-          round(x[1,yr,1]*p[1,yr,1], digits = 2)
-)
-
-#Create table of results
-IO_Ind1<-as.data.frame(Ind1,row.names=rownames)
-kable(IO_Ind1)
-
-#Create industry 2 column
-Ind2 <- c(round(x[1,yr,2]*A[3]*p[1,yr,1], digits = 2),                                                                    
-           round(x[1,yr,2]*A[4]*p[1,yr,2], digits = 2),
-           round(x[1,yr,2]*p[1,yr,2]-(x[1,yr,2]*A[3]*p[1,yr,1]+x[1,yr,2]*A[4]*p[1,yr,2]), digits = 2),
-           round(x[1,yr,2]*p[1,yr,2], digits = 2)
-)
-
-#Create table of results
-IO_Ind2<-as.data.frame(Ind2,row.names=rownames)
-kable(IO_Ind2)
-
-#Create final demand column
-Dem <- c(round(d[1,yr,1]*p[1,yr,1], digits = 2),                                                                    
-         round(d[1,yr,2]*p[1,yr,2], digits = 2),
-         round((d[1,yr,1]*p[1,yr,1]+d[1,yr,2]*p[1,yr,2]), digits = 2),
-         "")
-
-
-#Create table of results
-IO_Dem<-as.data.frame(Dem,row.names=rownames)
-kable(IO_Dem)
-
-#Create output column
-Out <- c(round(x[1,yr,1]*A[1]*p[1,yr,1]+x[1,yr,2]*A[3]*p[1,yr,1]+d[1,yr,1]*p[1,yr,1], digits = 2),                                                                    
-         round(x[1,yr,1]*A[2]*p[1,yr,2]+x[1,yr,2]*A[4]*p[1,yr,2]+d[1,yr,2]*p[1,yr,2], digits = 2),
-         "",
-         round(p[1,yr,1]*x[1,yr,1]+p[1,yr,2]*x[1,yr,2], digits = 2))
-
-
-#Create table of results
-IO_Out<-as.data.frame(Out,row.names=rownames)
-kable(IO_Out)
-
-#Create IO matrix
-IO_Matrix<-cbind(IO_Ind1,IO_Ind2,IO_Dem,IO_Out)
-kable(IO_Matrix) #Unload kableExtra to use this
-
-### HTML versions ################################################################################
+################################################################################
+################################################################################
 
 #Create html and latex tables
 
@@ -275,9 +251,8 @@ kable(IO_Matrix) #Unload kableExtra to use this
 library(kableExtra)
 
 #Create captions
-caption1 <- paste("Table 1. Balance sheet in period", yr, "under baseline")
-caption2 <- paste("Table 2. Transactions-flow matrix in period ",yr, "under baseline")
-caption3 <- paste("Table 3. Input-output matrix in period ",yr, "under baseline")
+caption1 <- paste("Table 1. Balance sheet of Model PC in period", yr, "under scenario", scen)
+caption2 <- paste("Table 2. Transactions-flow matrix of Model PC in period ",yr, "under scenario", scen)
 
 #Create html table for BS
 BS_Matrix %>%
@@ -294,13 +269,5 @@ TFM_Matrix %>%
       format= "html",
       #format= "latex",
       col.names = c("Households","Firms","Central bank","Government","Row total"),
-      align="r") %>%
-  kable_classic(full_width = F, html_font = "helvetica")
-
-#Create html table for IO
-IO_Matrix %>%
-  kbl(caption=caption3,
-      format= "html",
-      col.names = c("Industry 1 (demand)","Industry 2 (demand)","Final demand","Output"),
       align="r") %>%
   kable_classic(full_width = F, html_font = "helvetica")
